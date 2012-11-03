ad1ebab2a5dfb2b2ebefe9a6c19ea8c14b033b44
Eugenio Estrada <mail@eugenioestrada.es>
2012-11-03 16:18:52 +0100

    Simple chat

--- server.js
+++ server.js
@@ -1,7 +1,91 @@
-var http = require('http');
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs'),
+    qs = require('querystring');
+
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"};
+
+// Helpers
+
+if (typeof String.prototype.startsWith != 'function') {
+  // see below for better implementation!
+  String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) == str;
+  };
+}
+
+if (typeof String.prototype.endsWith != 'function') {
+  String.prototype.endsWith = function (str){
+    return this.slice(-str.length) == str;
+  };
+}
+
+// Actions
+
+var HomeAction = "/";
+var NewMessagesAction = "/NewMessages";
+var SendMessageAction = "/SendMessage";
+var RegisterAction = "/Register";
+
+var messages = [ { message: "Bienvenidos al Chat de Difoosion", timestamp: (new Date).getTime() } ];
+var buddies = [ ];
+
 var requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, HTTP!\n');
+	var body = "";
+	req.on("data", function(chunk){
+		body += chunk;
+	});
+
+  	var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd() + "\\static_files", uri);
+
+    path.exists(filename, function(exists) {
+        if ( filename.endsWith("\\") || !exists ) {
+			if (req.url.startsWith(NewMessagesAction)) {
+				res.writeHead(200, { 'Content-Type': 'application/json' }); 
+		  		res.write(JSON.stringify(messages));
+		  		res.end();
+			}
+			if (req.url.startsWith(SendMessageAction)) {
+				if (req.method == 'POST') {
+					var bodyJson = qs.parse(body);
+				    messages.push({ message : bodyJson.message, timestamp: (new Date).getTime()});
+					res.writeHead(200);
+			  		res.end("OK!");	
+				}
+				else {
+					res.writeHead(403);
+			  		res.end("Método no permitido");		
+				}
+			}
+			if (req.url.startsWith(RegisterAction)) {
+				res.writeHead(200);
+		  		res.end("Register");	
+			}
+			else if (req.url == HomeAction) {
+		        res.writeHead(200, mimeType);
+		        var fileStream = fs.createReadStream(path.join(process.cwd() + "\\static_files", "index.html"));
+		        fileStream.pipe(res);
+			}
+			else {
+				res.writeHead(404);
+				res.end("No encontrado.");
+			}
+        }
+        else {
+	        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+	        res.writeHead(200, mimeType);
+	        var fileStream = fs.createReadStream(filename);
+	        fileStream.pipe(res);
+    	}
+    });
 };
 
 var server = http.createServer(requestListener);
